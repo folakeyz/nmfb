6fcc4c27749bfa6a3789fcd989e2749e5388f595
iCode <gbengaobafemi1@gmail.com>
2021-06-03 19:31:03 +0100

    Second Upload

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "development") {
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/*", function (req, res) {
+app.get("/*", function (req, res) 
   res.sendFile(path.join(__dirname, "public/index.html"), function (err) {
     if (err) {
       res.status(500).send(err);
